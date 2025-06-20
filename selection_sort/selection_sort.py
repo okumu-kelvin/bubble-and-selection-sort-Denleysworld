c12b32bffeef6d4572921437119774ff66491f37
denley <deekayel05@gmail.com>
2025-06-20 13:34:58 +0300

    restrating selection

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,11 +1,2 @@
 def selection_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        min_index=i
-        for k in range(i+1,n):
-            if arr[k]<arr[min_index]:
-                min_index=k
-        
-        arr[i], arr[min_index] = arr[min_index], arr[i]
-    return arr
-  
+    
