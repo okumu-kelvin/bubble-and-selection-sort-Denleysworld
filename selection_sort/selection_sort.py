cbbe4e78d26f1c859fe7a8b4f0febc5ad16352b0
denley <deekayel05@gmail.com>
2025-06-20 13:32:17 +0300

    removing test case

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -2,11 +2,10 @@ def selection_sort(arr):
     n = len(arr)
     for i in range(n):
         min_index=i
-        for j in range(i+1,n):
-            if arr[j]<arr[min_index]:
-                min_index=j
+        for k in range(i+1,n):
+            if arr[k]<arr[min_index]:
+                min_index=k
         
         arr[i], arr[min_index] = arr[min_index], arr[i]
     return arr
   
-print(selection_sort([29, 10, 14, 37, 14])) 
